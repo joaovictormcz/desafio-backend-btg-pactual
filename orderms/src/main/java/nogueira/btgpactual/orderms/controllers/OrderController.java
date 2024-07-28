802b656ef301b7400a87dd998da778dd44e8d4c5
nogueira <joaovictor.mcz@gmail.com>
2024-07-28 15:19:22 -0300

    feat: Project build

--- orderms/src/main/java/nogueira/btgpactual/orderms/controllers/OrderController.java
+++ orderms/src/main/java/nogueira/btgpactual/orderms/controllers/OrderController.java
@@ -4,8 +4,11 @@ import nogueira.btgpactual.orderms.controllers.dto.ApiResponse;
 import nogueira.btgpactual.orderms.controllers.dto.OrderResponse;
 import nogueira.btgpactual.orderms.controllers.dto.PaginationResponse;
 import nogueira.btgpactual.orderms.service.OrderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,16 +20,23 @@ import java.util.Map;
 public class OrderController {
 
     private final OrderService orderService;
+    private static final Logger logger = LoggerFactory.getLogger(OrderController.class);
 
     public OrderController(OrderService orderService) {
         this.orderService = orderService;
     }
 
     @GetMapping("/customers/{customerId}/orders")
-    public ResponseEntity<ApiResponse<OrderResponse>> listOrders(@PathVariable("customerId") Long customerId,
+    public ResponseEntity<ApiResponse<OrderResponse>> listOrders(@PathVariable("customerId") @NonNull Long customerId,
                                                                  @RequestParam(name = "page", defaultValue = "0") Integer page,
                                                                  @RequestParam(name = "pageSize", defaultValue = "10") Integer pageSize) {
 
+        /*
+            In this example, `page` will be extracted from the URL `/customers/1/orders?page=1&pageSize=10`.
+            If `page` is not provided, `default` will be used as the default value."
+         */
+
+        logger.info("Listing orders for customerId: {}", customerId);
         var pageResponse = orderService.findAllByCustomerId(customerId, PageRequest.of(page, pageSize));
         var totalOnOrders = orderService.findTotalOrdersByCustomerId(customerId);
 
